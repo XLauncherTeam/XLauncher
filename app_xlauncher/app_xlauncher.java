8f35d1784f4cc4d905b7fa20efb31c2172041c62
AR-DEV-1 <115883839+AR-DEV-1@users.noreply.github.com>
2023-10-09 10:19:20 +0500

    Create app_xlauncher.java
    Signed-off-by: AR-DEV-1 <115883839+AR-DEV-1@users.noreply.github.com>

---
+++ app_xlauncher/app_xlauncher.java
@@ -0,0 +1,27 @@
+package io.github.xlauncherteam.xlauncher.*;
+package net.minecraft.clientJAR.*;
+
+import static io.github.xlauncherteam.xlauncher.*;
+import io.github.xlauncherteam.xlauncher.assets.*;
+import io.github.xlauncherteam.xlauncher.app_xlauncher.jar.*;
+
+import java.io.IOExeception.*;
+
+public class app_xlauncher {
+private static app_xlauncher startapp_xlauncher = public static null;
+public static app_xlauncher app = null;
+
+    public static void main(String[] args) {
+        
+        String minecraftExecutablePath = "";
+       
+        String javaArgs = "-Xmx2G";
+  try {
+
+
+
+}catch{
+
+    }
+  }
+}
